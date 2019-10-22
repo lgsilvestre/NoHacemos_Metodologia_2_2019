62e178c08ac6d5844e1ec3d366db04e7ce06614d
Shipus <jjimenez17@alumnos.utalca.cl>
2019-10-21 21:36:23 -0300

    Arreglos menores Se agrega la funcion de reproduccion Se agrega imagen de prueba se arregla problemas de reproduccion

--- TriviflexPlus/src/Principal/Contr_rpeliculas.java
+++ TriviflexPlus/src/Principal/Contr_rpeliculas.java
@@ -5,6 +5,7 @@
  */
 package Principal;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
@@ -12,6 +13,10 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import static javafx.scene.media.MediaPlayer.Status.PLAYING;
+import javafx.scene.media.MediaView;
 
 /**
  *
@@ -21,7 +26,26 @@ public class Contr_rpeliculas implements Initializable{
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        File file = new File("bueno.mp4");
+        media = new Media(file.toURI().toString());
+        media.setOnError(() -> System.out.println("error media"));
+
+        player = new MediaPlayer(media);
+        player.setOnError(() -> System.out.println("error player"));
+        
+        
+        if(view == null){
+            view = new MediaView(player);
+        }
+        view.toFront();
+        view.setMediaPlayer(player);
+        
+        
     }
+    @FXML
+    private Media media;
+    @FXML
+    private MediaPlayer player;
     @FXML
     private AnchorPane root;
     @FXML
@@ -36,31 +60,64 @@ public class Contr_rpeliculas implements Initializable{
     private Button b4;
     @FXML
     private Button b5;
+    @FXML
+    private MediaView view;
+    @FXML
+    private Button bplay;
+    @FXML
+    private Button bstop;
     
     @FXML
     public void volver(){
+        if(player.getStatus()==PLAYING){
+            player.stop();
+        }
         SceneHandler.cargarVista(root, getClass().getResource("/Vistas/principal.fxml"));
     }
     @FXML
     public void buscar(){
+        if(player.getStatus()==PLAYING){
+            player.stop();
+        }
         SceneHandler.cargarVista(root, getClass().getResource("/Vistas/busqueda.fxml"));
     }
     @FXML
     public void peliculas(){
+        if(player.getStatus()==PLAYING){
+            player.stop();
+        }
         SceneHandler.cargarVista(root, getClass().getResource("/Vistas/peliculas.fxml"));
     }
     @FXML
     public void series(){
+        if(player.getStatus()==PLAYING){
+            player.stop();
+        }
         SceneHandler.cargarVista(root, getClass().getResource("/Vistas/series.fxml"));
     }
     @FXML
     public void musica(){
+        if(player.getStatus()==PLAYING){
+            player.stop();
+        }
         SceneHandler.cargarVista(root, getClass().getResource("/Vistas/musica.fxml"));
     }   
+    @FXML
+    public void play(){
+        if(player.getStatus()==PLAYING){
+            player.stop();
+        }
+        player.play();
+    }
+    @FXML
+    public void stop(){
+        player.stop();
+    }
     /***
      * TDO
      * Actualizar con reproduccion de video
      */
+    
     
     
 }
